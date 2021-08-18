f829ade9649252d09b91d29ca5d95ea8ac571033
Nikita Podrezov <leronimxd@gmail.com>
2021-08-18 14:36:38 +0700

    feat: initial commit

---
+++ android/src/main/java/com/reactnativebip32/Bip32Module.kt
@@ -0,0 +1,47 @@
+package com.reactnativebip32
+
+import android.net.Network
+import android.text.TextUtils
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.bridge.ReactContextBaseJavaModule
+import com.facebook.react.bridge.ReactMethod
+import com.facebook.react.module.annotations.ReactModule
+import com.reactnativebip32.Bip32Module
+
+@ReactModule(name = Bip32Module.NAME)
+class Bip32Module(reactContext: ReactApplicationContext?) : ReactContextBaseJavaModule(reactContext) {
+  override fun getName(): String {
+    return NAME
+  }
+
+  companion object {
+    const val NAME = "Bip32"
+
+    init {
+      try {
+        // Used to load the 'native-lib' library on application startup.
+        System.loadLibrary("cpp")
+      } catch (ignored: Exception) {
+      }
+    }
+  }
+
+  fun getHardenedPath(path: String): String {
+    val t: ArrayList<String?> = ArrayList();
+    val splitPath = path.split("/").toTypedArray()
+    for (i in 0..splitPath.size) {
+      if (splitPath[i] == "m" || splitPath[i].indexOf("'") != -1) {
+        t.add(splitPath[i]);
+      }
+    }
+    return t.joinToString(separator = "/")
+  }
+
+  // Example method
+  // See https://reactnative.dev/docs/native-modules-android
+  @ReactMethod
+  fun sign(seed: String, network: Network?, path: String?) {
+    val seedByte = seed.toByteArray()
+    //        ExtendedPrivateKey key = ExtendedPrivateKey.fromSeed(seedByte, network);
+  }
+}
---
+++ android/src/main/java/com/reactnativebip32/Bip32Package.kt
@@ -0,0 +1,19 @@
+package com.reactnativebip32
+
+import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.uimanager.ViewManager
+import java.util.*
+
+class Bip32Package : ReactPackage {
+  override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
+    val modules: MutableList<NativeModule> = ArrayList()
+    modules.add(Bip32Module(reactContext))
+    return modules
+  }
+
+  override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
+    return emptyList()
+  }
+}
